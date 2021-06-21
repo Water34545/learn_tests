b5b5972625ccf4dbe9b684c866828407b90decbf
water <water34545@yandex.ru>
2021-06-21 11:27:34 +0300

    Testing Rendering

--- src/Post/post.spec.js
+++ src/Post/post.spec.js
@@ -1,28 +1,29 @@
 import React from "react";
 import Post from "./post";
 
-const setUp = (props) => shallow(<Post {...props} />);
+const setUp = (props) => shallow(<Post {...props}/>)
 
 describe("should render Post component", () => {
-  let component;
+  let component
+
   beforeEach(() => {
-    component = setUp();
-  });
+    component = setUp()
+  })
 
-  it("should contain .post wrapper", () => {
-    const wrapper = component.find(".post");
-    expect(wrapper.length).toBe(1);
+  it("should render .post wrapper", () => {
+    const wrapper = component.find('.post')
+    expect(wrapper.length).toBe(1)
   });
-
+  
   it("should contain link", () => {
-    const wrapper = component.find("a");
-    expect(wrapper.length).toBe(1);
+    const wrapper = component.find('a')
+    expect(wrapper.length).toBe(1)
   });
 
   it("should render created date", () => {
-    const created_at = "01-03-2020";
-    component = setUp({ created_at });
-    const date = component.find(".date");
-    expect(date.text()).toBe(new Date(created_at).toLocaleDateString());
+    const created_at = '01-03-2021'
+    component = setUp({created_at})
+    const wrapper = component.find('.date')
+    expect(wrapper.text()).toBe(new Date(created_at).toLocaleDateString())
   });
-});
+})
