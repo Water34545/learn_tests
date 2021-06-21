c8cc08324f3a3b6fcfc30e4c1c5a65203c5f2e3e
water <water34545@yandex.ru>
2021-06-21 14:10:40 +0300

    Testing Change Events

--- src/Posts/posts.spec.js
+++ src/Posts/posts.spec.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import Posts from './posts'
 
+const setUp = () => shallow(<Posts/>)
+
 describe('Post components', () => {
+  const DEFAULT_PAGE = 10;
+  let component;
+  let instance;
+
+  beforeEach(() => {
+    component = setUp();
+    instance = component.instance()
+  })
+
   it('should render post component', () =>{
-    const component = shallow(<Posts/>)
     expect(component).toMatchSnapshot()
+  })
+
+  describe('Post handlers', () => {
+    it('should handel search input value', () =>{
+      expect(component.state().searchQuery).toBe('')
+      instance.handleInputChange({target: {value: 'test'}})
+      expect(component.state().searchQuery).toBe('test')
+    })
+
+    it('should handel change per page', () =>{
+      expect(component.state().hitsPerPage).toBe(20)
+      instance.handleHitsChange({target: {value: String(DEFAULT_PAGE)}})
+      expect(component.state().hitsPerPage).toBe(10)
+    })
+
+    it('should handel change per page if clicked "Enter"', () =>{
+      instance.setState({page: DEFAULT_PAGE})
+      instance.getSearch({key: 'Enter'})
+      expect(component.state().page).toBe(0)
+    })
+
+    it('should handel change per page if clicked "a"', () =>{
+      instance.setState({page: DEFAULT_PAGE})
+      instance.getSearch({key: 'a'})
+      expect(component.state().page).toBe(DEFAULT_PAGE)
+    })
   })
 })
--- src/Title/title.spec.js
+++ src/Title/title.spec.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Title from './title'
 
-describe('Ешеду component', () => {
-  it('should render Ешеду component with props', () =>{
+describe('Title component', () => {
+  it('should render Title component with props', () =>{
     const component = shallow(<Title title='test '/>)
     expect(component).toMatchSnapshot()
   })
-  it('should render Ешеду component without props', () =>{
+  it('should render Title component without props', () =>{
     const component = shallow(<Title/>)
     expect(component).toMatchSnapshot()
   })
