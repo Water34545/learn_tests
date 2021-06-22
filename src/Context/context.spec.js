35fb4d95e8be2d53e0111dbbaa8b7e1c311a97d7
water <water34545@yandex.ru>
2021-06-23 00:50:48 +0300

    Context & Portal Testing

---
+++ src/Context/context.spec.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { AuthProvider } from "./context";
+import ContextComponent from "./component";
+
+describe('ContextComponent component', () => {
+  const component = mount(<AuthProvider><ContextComponent/></AuthProvider>)
+
+  it('soult loggin toggle status', () => {
+    expect(component.find('div').text()).toBe('false')
+    component.find('.btn').simulate('click')
+    expect(component.find('div').text()).toBe('true')
+
+    component.find('.btn').simulate('click')
+    expect(component.find('div').text()).toBe('false')
+  })
+})
